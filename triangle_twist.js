f86bccb84f2316e0d60e327df9244e1c3dda0290
longtd1976 <longtranduc1976@gmail.com>
2015-07-19 13:10:47 +0700

    a

--- triangle_twist.js
+++ triangle_twist.js
@@ -39,7 +39,7 @@ function init()
 
     bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, 8*Math.pow(3, 12), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, 8*Math.pow(3, 15), gl.STATIC_DRAW );
 
 
 
